132a5936fded0ae3bd52a428099aeb46a76a00f0
Kosk0l <kosk0l@yandex.ru>
2025-10-31 17:04:47 +0300

    The Server is  working

--- cmd/grpc/main.go
+++ cmd/grpc/main.go
@@ -1,26 +1,68 @@
 package main
 
 import (
+	grpcServer "GoCrudGrpc/internal/grpc"
+	"GoCrudGrpc/internal/storage"
+	pb "GoCrudGrpc/proto/grpc"
+	"context"
 	"log"
 	"net"
-	pb "GoCrudGrpc/proto/grpc"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 	"google.golang.org/grpc"
-	grpcServer "GoCrudGrpc/internal/grpc"
 )
 
 
 func main() {
-	lis, err := net.Listen("tcp", ":44044") // запуск слушателя порта
+
+	// Базовый контекст
+	// Только на этапе инициализации — когда создаём подключение к базе (pgxpool.NewWithConfig).
+	// нужен свой собственный контекст, чтобы:
+	// 	1. Если БД не отвечает 5 секунд, не висеть вечно;
+	// 	2. Корректно завершить подключение при запуске;
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	// Ключ подключения
+	dsn := "postgres://user:password@localhost:5432/dbname"
+
+	// Создание подключение
+	db, err := storage.NewPostgres(ctx, dsn)
+	if err != nil {
+		log.Fatalf("failed to connect to postgres: %v", err)
+	}
+	defer db.Close()
+
+	log.Println("Connected to Postgres")
+
+	//======================================================================//
+
+	// Иннициализация слушателя порта
+	lis, err := net.Listen("tcp", ":44044") 
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 
-	s := grpc.NewServer() // Иннициализация сервера
+	// Иннициализация сервера
+	s := grpc.NewServer()
+
+	// Передача подключение к БД в слой gRPC
+	// реализация АПИ // регистрация сервера
+	serverAPI := grpcServer.NewServerAPI(db)
+	pb.RegisterGRPCServer(s, serverAPI)
+
+	// Запуск Сервера в отдельной горутине
+	go func() {
+		if err := s.Serve(lis); err != nil {
+			log.Fatalf("failed to serve: %v", err)
+		}
+	}()
 
 	log.Println("Server is working")
-	pb.RegisterGRPCServer(s, &grpcServer.ServerAPI{}) // реализация АПИ // регистрация сервера
-
-	if err := s.Serve(lis); err != nil { 
-		log.Fatalf("failed to serve: %v", err)
-	}
+	// Ожидание сигналов завершения (Ctrl+C)
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, os.Interrupt, syscall.SIGTERM)
+	<-sig
 }
