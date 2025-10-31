c9f2802b75c6d4c32a87ac7e74023294efe71ea7
Kosk0l <kosk0l@yandex.ru>
2025-10-31 16:43:34 +0300

    gRPC just connecting Postgres storage

--- internal/grpc/serve.go
+++ internal/grpc/serve.go
@@ -1,6 +1,9 @@
 package grpc
 
+// gRPC слой не должен знать, как именно хранится информация — только делегировать работу вниз.
+
 import (
+	"GoCrudGrpc/internal/storage"
 	"GoCrudGrpc/proto/grpc"
 	"context"
 
@@ -8,26 +11,15 @@ import (
 	"google.golang.org/grpc/status"
 )
 
-// структура для обработки запросов
-type Response interface {
-	Create(
-		ctx context.Context,
-		userID int64,
-		text string,
-	) (*grpc.MessageResponse, error)
-	Get(
-		ctx context.Context,
-		id int64,
-	) (*grpc.MessageResponse, error)
-	Delete(
-		ctx context.Context,
-		id int64,
-	) error
-}
-
 // Структура, наследующая интерфейс со всеми методами - хендлерами
 type ServerAPI struct {
     grpc.UnimplementedGRPCServer // наследует генерированные данные
+	storage *storage.Postgres // зависимость с БД // указатель на память объекта Структуры
+}
+
+// Конструктор для ServerAPI // Передает указатель
+func NewServerAPI(storage *storage.Postgres) *ServerAPI {
+	return &ServerAPI{storage: storage}
 }
 
 //===================================================================================================================//
@@ -36,16 +28,16 @@ type ServerAPI struct {
 // Получить сообщение из БД
 func (s *ServerAPI) GetMessage(ctx context.Context, req *grpc.GetMessageRequest) (*grpc.MessageResponse, error) {
 	if req.GetTextID() == 0 {
-		return nil, status.Error(codes.InvalidArgument, "Bad text id")
+		return nil, status.Error(codes.InvalidArgument, "Bad text id") // Неверные аргументы в запросе.
 	}
 
-	//..
+	// Проходка в Базу Данных
+	Response, err := s.storage.Get(ctx, req.TextID) 
+	if err != nil {
+		return nil, status.Error(codes.Internal, "Error DataBase Get") // Внутренняя ошибка сервера
+	}
 
-    return &grpc.MessageResponse{
-        TextID: req.TextID,
-        Text:   "",
-        Status: "",
-    }, nil
+    return Response, nil
 }
 
 // Добавить сообщение в бд
@@ -62,13 +54,12 @@ func (s *ServerAPI) CreateMessage(ctx context.Context, req *grpc.PostMessageRequ
 		return nil, status.Error(codes.InvalidArgument, "Bad text")
 	}
 
-	//..
+	Response, err := s.storage.Create(ctx, req)
+	if err != nil {
+		return nil, status.Error(codes.Internal, "Error DataBase Create") 
+	}
 
-	return &grpc.MessageResponse {
-        TextID: req.TextID,
-        Text:   "",
-        Status: "",
-    }, nil
+	return Response, nil
 }
 
 // Обновить сообщение в бд
@@ -81,13 +72,12 @@ func (s *ServerAPI) UpdateMessage(ctx context.Context, req *grpc.UpdateMessageRe
 		return nil, status.Error(codes.InvalidArgument, "Bad text id")
 	}
 
-	//..
+	Response, err := s.storage.Update(ctx, req)
+	if err != nil {
+		return nil, status.Error(codes.Internal, "Error DataBase Create") 
+	}
 
-	return &grpc.MessageResponse{
-        TextID: req.TextID,
-        Text:   "",
-        Status: "",
-    }, nil
+	return Response, nil
 }
 
 // Удалить сообщение из бд
@@ -96,11 +86,14 @@ func (s *ServerAPI) DeleteMessage(ctx context.Context, req *grpc.DeleteMessageRe
 		return nil, status.Error(codes.InvalidArgument, "Bad text id")
 	}
 
-	//..
+	err := s.storage.Delete(ctx,req)
+	if err != nil {
+		return nil, status.Error(codes.Internal, "Error DataBase Create") 
+	}
 
 	return &grpc.MessageResponse{
         TextID: req.TextID,
-        Text:   "",
-        Status: "",
+        Text:   "Message Deleted",
+        Status: "Message Deleted",
     }, nil
 }
